98256d9e7c18e1b2ab11ee869a640f1a7900d472
Bang Yongsu <yongsu9630@gmail.com>
2023-12-06 19:13:36 +0900

    12.06 19:13

--- ZeroNine/src/main/java/com/zeronine/dto/OBoardImageVO.java
+++ ZeroNine/src/main/java/com/zeronine/dto/OBoardImageVO.java
@@ -1,12 +1,34 @@
 package com.zeronine.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter@Getter
 public class OBoardImageVO {
 	private String oBoardImageId;
 	private String oBoardId;
 	private String oBoardImagePath;
 	private String oBoardImageType;
+	
+	public String getoBoardImageId() {
+		return oBoardImageId;
+	}
+	public void setoBoardImageId(String oBoardImageId) {
+		this.oBoardImageId = oBoardImageId;
+	}
+	public String getoBoardId() {
+		return oBoardId;
+	}
+	public void setoBoardId(String oBoardId) {
+		this.oBoardId = oBoardId;
+	}
+	public String getoBoardImagePath() {
+		return oBoardImagePath;
+	}
+	public void setoBoardImagePath(String oBoardImagePath) {
+		this.oBoardImagePath = oBoardImagePath;
+	}
+	public String getoBoardImageType() {
+		return oBoardImageType;
+	}
+	public void setoBoardImageType(String oBoardImageType) {
+		this.oBoardImageType = oBoardImageType;
+	}
 }
