a7c5e25e64883e138e7ac6392a5934bbe3aeb8ee
Chung JooYoung <hyundia1947@naver.com>
2023-12-06 18:01:55 +0900

    12/06 18:01 board edit 생성 및 수정

--- ZeroNine/src/main/webapp/static/js/dropbox.js
+++ ZeroNine/src/main/webapp/static/js/dropbox.js
@@ -1,38 +1,36 @@
 window.onload=()=>{
-	dropdown = () => {
+      document.querySelector('.dropbtn_click').onclick = ()=>{
+        dropdown();
+      }
+      document.getElementsByClassName('fastfood').onclick = ()=>{
+        showMenu(value);
+      };
+      dropdown = () => {
         var v = document.querySelector('.dropdown-content');
         var dropbtn = document.querySelector('.dropbtn')
         v.classList.toggle('show');
         dropbtn.style.borderColor = 'rgb(94, 94, 94)';
       }
-      
-      document.querySelector('.dropdown_btn').onclick = ()=>{
-        dropdown();
-      }
-      
-      document.getElementsByClassName('boardtype').onclick = ()=>{
-        showbType(value);
-      };
-      
 
-      showbType=(value)=>{
-        var dropbtn_content = document.querySelector('.dropdown-content');
-        var dropdown_btn = document.querySelector('.dropdown_btn');
+      showMenu=(value)=>{
+        var dropbtn_icon = document.querySelector('.dropbtn_icon');
+        var dropbtn_content = document.querySelector('.dropbtn_content');
+        var dropbtn_click = document.querySelector('.dropbtn_click');
         var dropbtn = document.querySelector('.dropbtn');
 
-        
+        dropbtn_icon.innerText = '';
         dropbtn_content.innerText = value;
         dropbtn_content.style.color = '#252525';
         dropbtn.style.borderColor = '#3992a8';
       }
     }
     window.onclick= (e)=>{
-      if(!e.target.matches('.dropdown_btn')){
+      if(!e.target.matches('.dropbtn_click')){
         var dropdowns = document.getElementsByClassName("dropdown-content");
 
-       
+        var dropbtn_icon = document.querySelector('.dropbtn_icon');
         var dropbtn_content = document.querySelector('.dropbtn_content');
-        var dropbtn_click = document.querySelector('.dropdown_btn');
+        var dropbtn_click = document.querySelector('.dropbtn_click');
         var dropbtn = document.querySelector('.dropbtn');
 
         var i;
