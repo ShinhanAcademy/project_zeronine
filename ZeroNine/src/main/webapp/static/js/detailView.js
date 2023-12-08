2f13ce05a575b435b04d241bbb4b96821a3b2b64
Chung JooYoung <hyundia1947@naver.com>
2023-12-08 15:57:14 +0900

    12/08 15:56 common >> detailView 수정

--- ZeroNine/src/main/webapp/static/js/detailView.js
+++ ZeroNine/src/main/webapp/static/js/detailView.js
@@ -1,21 +1,28 @@
-const btn = document.querySelector("#detail_btn");
-const modal = document.querySelector("#detail_modal_wrap");
-const close_btn = document.querySelector(".close_btn");
-const join_btn = document.querySelector(".join_btn");
 
-btn.onclick = function(){
+
+//fast_board
+function f_btn(){
 $("#detail_modal_wrap").show();
 }
 
-close_btn.onclick = function(){
-modal.style.display="none";
+//free_delivery
+function fd_btn(){
+$("#detail_modal_wrap").show();
 }
 
+//one_to_one_board
+function o_btn() {
+$("#detail_modal_wrap").show();
+}
 
+//close
+function close_btn(){
+$("#detail_modal_wrap").hide();
+}
 
 //modal_body 를 제외한 부분을 클릭시 취소
 window.onclick = function(){
-	if(event.target == modal){
-	modal.style.display="none";
+	if($(event.target).is("#detail_modal_wrap")){
+	$("#detail_modal_wrap").hide();
 }
 }
