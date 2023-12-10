ee6926f8faad8318cebd2276e6c07aec9360ba91
DESKTOP-B021BME\Eunkyung <eunkyung211@naver.com>
2023-12-10 16:26:33 +0900

    - 마이페이지

---
+++ ZeroNine/src/main/webapp/static/js/myPage/myShopping.js
@@ -0,0 +1,64 @@
+/*	my page -orderHistory */
+let currentDate = new Date();
+
+// Calendar(air-datepicker.js)
+$(".mypage_container #datepicker").datepicker({
+	language: "ko",
+	toggleSelected: false,
+    maxDate: new Date(),
+});
+$(".mypage_container #datepicker").data("datepicker").selectDate(new Date(currentDate.setMonth(currentDate.getMonth() - 1)));
+$(".mypage_container #datepicker").data("datepicker").selectDate(new Date());
+$(".search_wrap .period button").click(function(){
+	currentDate = new Date();
+	$(".mypage_container #datepicker").data("datepicker").clear();
+	$(".search_wrap .period button").removeClass("on");
+	$(this).addClass("on");
+	if($(this).attr("period") == "1") {
+		$(".mypage_container #datepicker").data('datepicker').selectDate(new Date(currentDate.setMonth(currentDate.getMonth() - 1)));
+	} else if($(this).attr("period") == "3") {
+		$(".mypage_container #datepicker").data('datepicker').selectDate(new Date(currentDate.setMonth(currentDate.getMonth() - 3)));
+	} else if($(this).attr("period") == "6") {
+		$(".mypage_container #datepicker").data('datepicker').selectDate(new Date(currentDate.setMonth(currentDate.getMonth() - 6)));
+	}
+	$(".mypage_container #datepicker").data("datepicker").selectDate(new Date());
+});
+
+// delivery modal
+$(".btn_delivery_check").on("click", function(){
+	$(".delivery_modal_wrap").show();
+		
+	//스크롤 제어
+	$('.delivery_modal_wrap').on('scroll touchmove mousewheel', function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
+	});
+});
+
+$(".delivery_modal_wrap .btn_close").on("click", function(){
+	closeModal();
+});
+	
+function closeModal() {
+	$('.delivery_modal_wrap').hide();
+	//스크롤 제어 해제
+	$('.delivery_modal_wrap').off('scroll touchmove mousewheel');
+}
+
+// $(document).mouseup(function (e){
+// 	console.log("eeeeevent",e);
+// 	console.log("?????", $(".delivery_modal_wrap .contents").has(e.target).length);
+// 	if($(".delivery_modal_wrap .contents").has(e.target).length == 0){
+// 		//closeModal();
+// 	};
+// });
+
+// ESC key 이벤트
+$(document).keydown(function(e){
+    let code = e.keyCode || e.which;
+
+    if (code == 27) { 
+		closeModal();
+    }
+});
