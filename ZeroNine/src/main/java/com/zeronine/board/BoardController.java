3b9d62aa18234796c55f6849714e3e30b99a8a4b
seunggwang <sgroh1997@gmail.com>
2023-12-16 01:50:58 +0900

    Mock Image data AWS 업로드 구현

--- ZeroNine/src/main/java/com/zeronine/board/BoardController.java
+++ ZeroNine/src/main/java/com/zeronine/board/BoardController.java
@@ -1,8 +1,17 @@
 package com.zeronine.board;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
@@ -12,16 +21,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import com.amazonaws.util.IOUtils;
 import com.zeronine.model.BoardService_jy;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
 	@Autowired
 	BoardService_jy boardService;
 	Logger logger = LoggerFactory.getLogger(BoardController.class);
-
+	private final S3Upload s3Upload;
+	
 	// board_Main
 	@RequestMapping("/")
 	public String boardListGet() {
@@ -92,9 +108,38 @@ public class BoardController {
 	}
 
 	@RequestMapping("/completeedit.do")
-	public String compliteEdit(@RequestParam("send_bt_to_com") String boardListType, Model model) {
+	public String compliteEdit(@RequestParam("send_bt_to_com")String boardListType, Model model ) throws IOException {
 		String lower_boardListType = boardListType.toLowerCase();
 		model.addAttribute("boardListType", lower_boardListType);
+		//view에서 (board type이 oneTooneBoard 이라면) 이미지가 전달되었음을 가정함.
+		if(lower_boardListType.equals("onetooneboard")) {
+			
+			//임시로 사용할 MultipartFile 생성
+			File file = new File("C:\\Users\\rohfr\\Downloads\\puppy.jpg");
+			logger.info("FILE NAME==>" + file.getName());
+			String extensionName = file.getName().substring(file.getName().lastIndexOf('.') + 1);
+			logger.info("extension Name ==>", extensionName);
+	        FileItem fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
+	        try {
+	            InputStream input = new FileInputStream(file);
+	            OutputStream os = fileItem.getOutputStream();
+	            IOUtils.copy(input, os);
+	        } catch (IOException ex) {
+	        	ex.printStackTrace();
+	        }
+	        MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
+			//**임시로 사용할 MultipartFile 생성
+	        
+	        
+	        String uuidStr = UUID.randomUUID().toString();
+	        String s3ImageURL = s3Upload.upload(multipartFile, uuidStr, extensionName);
+	        logger.info(s3ImageURL);
+	        /*
+	         UUID는 oBoardId가 됨, s3ImageURL은 해당 oBoard의 이미지 링크
+	         oBoard 생성할 때 uuid가 uuidStr로 외부에서 주입된 값으로 지정되어 INSERT 해야함
+	       	 oBoardImage에는 boardId(uuidStr) - URL(s3ImageURL) 쌍이 들어감
+	         * */
+		}
 		System.out.println("controller에서 게시글 작성 후 넘어가는 보드 타입 알고싶어~~" + boardListType);
 		return "board/completeEdit";
 	}
