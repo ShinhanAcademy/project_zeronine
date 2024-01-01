9762ae30f1171a0db54895de6ba9d00b17a5c7c2
DESKTOP-3O6KIK7\User <eunkyung211@naver.com>
2024-01-02 03:49:00 +0900

    - 메인 수정

--- ZeroNine/src/main/webapp/static/js/jslib/chart.js
+++ ZeroNine/src/main/webapp/static/js/jslib/chart.js
@@ -2,20 +2,35 @@
 //    draw(80, '.pie-chart', '#F11B1B');
 // });
 
-function draw(max, classname, pointColorname, backgroundColorName){
-   var i=1;
-    var func1 = setInterval(function(){
+function draw(max, classname) {
+	let pointColor = "";
+	let backColor = "";
+	
+	if(max<35) {
+		pointColor = "#09244B";
+		backColor = "#7390BA";
+	} else if(max>=35 && max<70) {
+		pointColor = "#F1C21B";
+		backColor = "#F9E59E";
+	} else {
+		pointColor = "#F11B1B";
+		backColor = "#F99E9E";
+	}
+	
+	let i=1;
+    
+    var func1 = setInterval(function() {
       if(i<max){
-          color1(i,classname, pointColorname, backgroundColorName);
+          callColor(i, classname, pointColor, backColor);
           i++;
       } else{
         clearInterval(func1);
       }
-    },10);
+    }, 10);
 };
 
-function color1(i, classname, pointColorname, backgroundColorName){
+function callColor(i, classname, pointColor, backColor){
    $(classname).css({
-      "background": `conic-gradient(${pointColorname} 0% ${i}%, ${backgroundColorName} ${i}% 100%)`
+      "background": `conic-gradient(${pointColor} 0% ${i}%, ${backColor} ${i}% 100%)`
    });
 };
